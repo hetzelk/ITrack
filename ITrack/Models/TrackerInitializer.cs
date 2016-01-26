f18d0f62012ec49f82ee611e4f38c239aac984c1
keithhetzel <keithh.8112@yahoo.com>
2016-01-26 17:15:57 -0600

    adding the two missing files

---
+++ ITrack/Models/TrackerInitializer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace ITrack.Models
+{
+    public class TrackerInitializer : DropCreateDatabaseIfModelChanges<Tracker>
+    {
+        protected override void Seed(Tracker context)
+        {
+            base.Seed(context);
+        }
+    }
+}
---
+++ ITrack/Models/UsersInitializer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace ITrack.Models
+{
+    public class UsersInitializer : DropCreateDatabaseIfModelChanges<Users>
+    {
+        protected override void Seed(Users context)
+        {
+            base.Seed(context);
+        }
+    }
+}
