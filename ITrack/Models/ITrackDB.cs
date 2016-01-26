31149c2da2832a4d0699b1575c09ddb1342661ab
keithhetzel <keithh.8112@yahoo.com>
2016-01-26 14:50:12 -0600

    added all initial files agains

---
+++ ITrack/Models/ITrackDB.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace ITrack.Models
+{
+    public class ITrackDB : DbContext
+    {
+        DbSet<Roles> roles { get; set; }
+    }
+}
---
+++ ITrack/Models/Roles.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace ITrack.Models
+{
+    public class Roles : DbContext
+    {
+        public int ID { get; set; }
+        public string Role { get; set; }
+    }
+}
