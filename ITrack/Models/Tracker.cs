48ae463267614f571fb58c2d63d57900f4d85df6
keithhetzel <keithh.8112@yahoo.com>
2016-01-27 12:18:52 -0600

    Changed the tracker database context

--- ITrack/Models/Tracker.cs
+++ ITrack/Models/Tracker.cs
@@ -9,6 +9,8 @@ namespace ITrack.Models
     public class Tracker 
     {
         public int ID { get; set; }
+        public string TicketID { get; set; }
+        public string Details { get; set; }
         public string TimeOut { get; set; }
         public string Location { get; set; }
         public string Employee { get; set; }
